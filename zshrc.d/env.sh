c0257d006437e0d2339dfe260f46b6439a851f4d
Li Yazhou <me.ssword@gmail.com>
2025-12-08 01:27:22 +0800

    refactor env.sh

--- zshrc.d/env.sh
+++ zshrc.d/env.sh
@@ -1,37 +1,49 @@
+#!/bin/bash
+
+# System & Locale
+export EDITOR=vim
+export TERM=xterm-256color
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 export QT_IM_MODULE=ibus
-export EDITOR=vim
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export TERM=xterm-256color
 
+# PATH
 export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH
-export PATH=$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH
-export PATH=$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH
 export PATH=$HOME/bin:$PATH
-export PATH="$HOME/.cargo/bin:$PATH"
 
+# Go
 export GOPATH=$HOME/go
 export GOROOT_BOOTSTRAP=$GOROOT
+export GOPROXY=https://goproxy.cn,direct
+export GOPRIVATE=*.zhenguanyu.com
 export PATH=$GOPATH/bin:/usr/local/opt/go/libexec/bin:$PATH
 
-# alias sudo="sudo env PATH=$PATH"
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
-export WASMER_DIR="$HOME/.wasmer"
-export JAVA_HOME=$(/usr/libexec/java_home)
-export GOPROXY=https://goproxy.io,direct
-export GOPRIVATE=*.zhenguanyu.com
-export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
-
-# eval "$(rbenv init - zsh)"
+# Python
+export PYENV_VERSION='3.13.3'
+export PIPENV_VENV_IN_PROJECT=1
+export PATH=$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-export PYENV_VERSION='3.13.3'
+# Ruby
 export RBENV_VERSION=2.3.0
-export PIPENV_VENV_IN_PROJECT=1
-export HOMEBREW_NO_AUTO_UPDATE=1
-export GOPROXY=https://goproxy.cn,direct
+export PATH=$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH
+# eval "$(rbenv init - zsh)"
+
+# Rust
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Java
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+# C/C++ Build
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
 export VCPKG_ROOT="$HOME/vcpkg"
+
+# Other Tools
+export WASMER_DIR="$HOME/.wasmer"
+export HOMEBREW_NO_AUTO_UPDATE=1
