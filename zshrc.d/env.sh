916c31dafbd7f1a9789387c968e2787b22d3555a
Li Yazhou <me.ssword@gmail.com>
2025-12-08 01:24:03 +0800

    fix

--- zshrc.d/env.sh
+++ zshrc.d/env.sh
@@ -6,14 +6,15 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export TERM=xterm-256color
 
-export GOPATH=$HOME/go
-export GOROOT_BOOTSTRAP=$GOROOT
-export PATH=$GOPATH/bin:/usr/local/opt/go/libexec/bin:$PATH
-
+export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH
 export PATH=$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH
 export PATH=$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH
 export PATH=$HOME/bin:$PATH
 export PATH="$HOME/.cargo/bin:$PATH"
+
+export GOPATH=$HOME/go
+export GOROOT_BOOTSTRAP=$GOROOT
+export PATH=$GOPATH/bin:/usr/local/opt/go/libexec/bin:$PATH
 
 # alias sudo="sudo env PATH=$PATH"
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
